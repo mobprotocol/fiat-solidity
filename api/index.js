d35364e2ec612d1b07bb9041e853dd9999041330
Sean Pollock <seanpollock3344@gmail.com>
2017-07-06 02:49:38 -0400

    adding client_token endpoint to create client key pair

--- api/index.js
+++ api/index.js
@@ -2,13 +2,19 @@ import express from 'express'
 
 const app = express()
 
+const gateway = braintree.connect({
+  environment: braintree.Evironment.sandbox,
+  merchantID: 'f7fk79dhpx4fwy93',
+  publicKey: 'b6t3ckmch6g695ss',
+  privateKey: 'b33c79621797622e43894fcd16286afc'
+})
+
 app.listen('4000', () => {
   console.log('### RESTFUL SERVER LISTENING ON PORT 4000')
 })
 
-const gateway = braintree.connect({
-  environment: braintree.Evironment.sandbox,
-  merchantID: "f7fk79dhpx4fwy93",
-  publicKey: "b6t3ckmch6g695ss",
-  privateKey: "b33c79621797622e43894fcd16286afc"
+app.get('/client_token', (req, res) => {
+  gateway.clientToken.generate({}, (err, res2) => {
+    res.send(res2.clientToken)
+  })
 })
