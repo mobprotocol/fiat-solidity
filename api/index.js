a417fe2670951eed0e6af9257f99f58f6f1a77fa
Sean Pollock <seanpollock3344@gmail.com>
2017-07-06 02:43:07 -0400

    Adding api authentication. Public, private key and merachant id

--- api/index.js
+++ api/index.js
@@ -2,7 +2,13 @@ import express from 'express'
 
 const app = express()
 
-
 app.listen('4000', () => {
   console.log('### RESTFUL SERVER LISTENING ON PORT 4000')
+})
+
+const gateway = braintree.connect({
+  environment: braintree.Evironment.sandbox,
+  merchantID: "f7fk79dhpx4fwy93",
+  publicKey: "b6t3ckmch6g695ss",
+  privateKey: "b33c79621797622e43894fcd16286afc"
 })
